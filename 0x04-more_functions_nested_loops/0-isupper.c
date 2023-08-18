08fd1e75ad361517a08fa8fedf7b4b243dc91bc5
aasemgar7i <aasemgar7i@gamil.com>
2023-08-18 03:20:24 +0300

    return

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/*
+* int _isupper(int c) : define only C
+*
+* Return: 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdio.h>
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
