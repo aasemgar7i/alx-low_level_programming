d2086a4d79d8b3aa9bb890fc5460b22f25fd9a43
aasemgar7i <aasemgar7i@gamil.com>
2023-08-21 22:07:33 +0300

    ordanairy

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - revers a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int end, start;
+	char d;
+
+	for (end = 0; s[end] != '\0'; end++)
+		;
+	end--;
+	for (start = 0; start < end; start++, end--)
+	{
+		d = s[start];
+		s[start] = s[end];
+		s[end] = d;
+	}
+}
