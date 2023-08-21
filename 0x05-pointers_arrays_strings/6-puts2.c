7fa3a9ac58a0c55d4da80a28b43304fcb8be9723
aasemgar7i <aasemgar7i@gamil.com>
2023-08-21 22:34:54 +0300

    all pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * @str: string to print the chars from
+ */
+
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (count % 2 != 0)
+			continue;
+		_putchar(str[count]);
+	}
+
+	_putchar('\n');
+}
