3e05daf4475598e555d771946c1c704b16672bac
aasemgar7i <aasemgar7i@gamil.com>
2023-08-25 04:20:22 +0300

    adv++

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* infinite_add - function
+* @n1: pointer 1
+* @n2: pointer 2
+* @r: pointer 3
+* @size_r: size pointer 3
+* Return: NULL
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (NULL);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - Prints function
+ * @b: output
+ * @size: size of buffer
+ * Return: NULL
+ */
+
+void print_buffer(char *b, int size)
+{
+	return (NULL);
+}
