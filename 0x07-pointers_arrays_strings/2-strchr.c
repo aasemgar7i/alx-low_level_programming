f7f770adfc7a07c91870ff8ba5fd3c506564fb0e
aasemgar7i <aasemgar7i@gamil.com>
2023-08-29 03:04:13 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (s);
+}
