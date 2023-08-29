4f341fa586822083635f8c420d264c64551408fa
aasemgar7i <aasemgar7i@gamil.com>
2023-08-29 03:54:59 +0300

    check for char

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strspn - set funcution
+* @s: see char
+* @accept: accept char
+* Return: 0
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	for (int i = 0 ; s[i] != '\0' ; ++i)
+	{
+		for (int j = 0 ; s[j] != accept[j] ; ++j)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (0);
+}
