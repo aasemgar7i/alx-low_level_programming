3c2eb2ce242c48bf12683efa2a9cec83721653c5
aasemgar7i <aasemgar7i@gamil.com>
2023-08-31 02:37:01 +0300

    x^y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _pow_recursion - x raised to the power of y
+* @x: input
+* @y: power
+* Return: result of the function
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recurstion(x, y - 1);
+}
