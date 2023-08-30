bb4c372cced57fafcfcd7c94d52d83eadf8f6e53
aasemgar7i <aasemgar7i@gamil.com>
2023-08-31 02:01:28 +0300

    hotfix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
+/**
+* _strlen_recursion - Entry point
+* @s: input
+* Return: Length
+*/
+
 int _strlen_recursion(char *s)
 {
+	if (*s == '\0')
+		return(0);
 	return (1 + _strlen_recursion(s + 1));
 }
