460e80b4ed47d10f7ae756528fd3d5fc6236063b
aasemgar7i <aasemgar7i@gamil.com>
2023-09-05 21:55:45 +0300

    old type

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - duplicate function
+* @str: input
+* Return: pointer mirror
+*/
+
+char *_strdup(char *str)
+{
+	int i, o;
+	char *mirror;
+
+	i = 0;
+	o = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[o])
+		o++;
+
+	mirror = malloc(sizeof(char *) * (o + 1));
+
+	if (mirror == NULL)
+		return (NULL);
+
+	while ((mirror[i] = str[i]) != '\0')
+		i++;
+
+	return (mirror);
+}
