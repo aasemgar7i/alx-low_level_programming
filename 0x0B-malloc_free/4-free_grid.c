5f019e50fbefc575fe09fe73a8c360dc93dc52d9
aasemgar7i <aasemgar7i@gamil.com>
2023-09-05 23:55:50 +0300

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - Entry point
+* @grid: input
+* @height: height of array
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
