b8c643ae656a5db2264ac2cdca96cbf3fedd5320
aasemgar7i <aasemgar7i@gamil.com>
2023-09-07 17:49:30 +0300

    good

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - memory allocation for intger b
+* @b: number of bytes
+* Return: pointer o
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *o
+
+	o = malloc(b);
+	if (o == NULL)
+		exit (98);
+
+	return (o);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+0x0C. C - More malloc, free
+
+
+    exit (3)
+    calloc
+    realloc
