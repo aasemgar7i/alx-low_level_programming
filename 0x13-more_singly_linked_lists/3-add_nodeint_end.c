a5ac88d676e1a1e9f59df19214b0294faf6bb468
aasemgar7i <aasemgar7i@gamil.com>
2023-09-26 08:43:33 +0300

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,9 +1,35 @@
 #include "lists.h"
 
 /**
-*
+* add_nodeint_end - main function
+* @head: output
+* @n: input
+* Return: new node
 */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *nnode, *cnode;
 
+	nnode = malloc(sizeof(listint_t));
+	if (nnode == NULL)
+		return (NULL);
+
+	nnode->n = n;
+	nnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nnode;
+		return (nnode);
+	}
+
+	cnode = *head;
+	while (cnode->next != NULL)
+	{
+		cnode = cnode->next;
+	}
+	cnode->next = nnode;
+
+	return (nnode);
+}
