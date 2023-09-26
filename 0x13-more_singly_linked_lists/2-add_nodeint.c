8e6761fd99cde0eb8df56c48f15e531255ff0747
aasemgar7i <aasemgar7i@gamil.com>
2023-09-26 08:32:37 +0300

    hotfix

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,7 @@
 * add_nodeint - main function
 * @head: output
 * @n: count of node
-* Return : node puts in start
+* Return: node puts in start
 */
 
 listint_t *add_nodeint(listint_t **head, const int n)
