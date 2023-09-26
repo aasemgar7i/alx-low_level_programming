2b6ed565f55264dc5612f48545092fbc380371d5
aasemgar7i <aasemgar7i@gamil.com>
2023-09-26 07:52:03 +0300

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* print_listint - main function
+* @h: input
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodenum = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodenum++;
+	}
+}
