ed51da026e37425d674f42056859e9dc72625fe4
aasemgar7i <aasemgar7i@gamil.com>
2023-09-26 08:23:37 +0300

    task2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* add_nodeint - main function
+* @head: output
+* @n: count of node
+* Return : node puts in start
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *atstart;
+
+	atstart = malloc(sizeof(listint_t));
+	if (atstart != NULL)
+		return (NULL);
+
+	atstart->n = n;
+	atstart->next = *head;
+	*head = atstart;
+
+	return (atstart);
+}
