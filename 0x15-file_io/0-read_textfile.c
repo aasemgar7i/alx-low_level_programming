cf0cf7ea2dec01604747ee056c75081b5d6a4f31
aasemgar7i <aasemgar7i@gamil.com>
2023-10-02 16:34:04 +0300

    hot fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,40 +13,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, rd, wr;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
 
-	rd = read(fd, buffer, letters);
-	if (rd == -1)
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	wr = write(STDOUT_FILENO, buffer, rd);
-	if (wr == -1 || wr != rd)
-	{
-		free(buffer);
-		close(fd);
 		return (0);
 	}
 
 	free(buffer);
-	close(fd);
-	return (wr);
+	close(o);
+
+	return (w);
 }
