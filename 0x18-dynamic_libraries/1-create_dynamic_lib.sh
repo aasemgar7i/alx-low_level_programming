3b9af874c1b23c18b03f55a5c1a54eb0aeabaeeb
aasemgar7i <aasemgar7i@gamil.com>
2023-12-12 00:31:01 +0200

    hf1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 c_files=$(ls *.c 2>/div/null)
-if [-z "#c_files" ]; then
-	echo "Nothing"
-	exit 1
-fi
-
 gcc -shared -fPIC -o liball.so $c_files
-
-if [$? -eq 0]; then
-	echo "File is Ready"
-else
-	echo "Err8"
-fi
