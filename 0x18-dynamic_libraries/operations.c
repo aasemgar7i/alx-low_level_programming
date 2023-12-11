2b43b6ed1e4cd412b6ff62e7d7afc058920f92cc
aasemgar7i <aasemgar7i@gamil.com>
2023-12-12 00:47:09 +0200

    thats was easy

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,30 @@
+int add(int q, int e);
+int sub(int q, int e);
+int mul(int q, int e);
+int div(int q, int e);
+int mod(int q, int e);
+
+int add(int q, int e)
+{
+	return (q + e);
+}
+
+int sub(int q, int e)
+{
+        return (q - e);
+}
+
+int mul(int q, int e)
+{
+        return (q * e);
+}
+
+int div(int q, int e)
+{
+        return (q / e);
+}
+
+int mod(int q, int e)
+{
+        return (q % e);
+}
